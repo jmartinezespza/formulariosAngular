5dd0153f40e03820f6d62535b33f516433c0966a
Jhonny Martínez Espinoza <jmartinezespza@gmail.com>
2021-07-31 00:16:23 -0500

    fomulario dinamico

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona {
+  nombre: string,
+  favorito: Favorito[];
+}
+
+interface Favorito {
+  id: number,
+  nombre: string
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
@@ -8,9 +18,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
+  persona: Persona = {
+    nombre: 'Jhonny',
+    favorito: [
+      { id: 1, nombre: 'Metal Gear'},
+      { id: 2, nombre: 'DeathStranidng'}
+    ]
+  }
+
+  nuevoJuego: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar() {
+    console.log('formulario posteado');
+    
+  }
+
+  eliminar(index: number ) {
+    this.persona.favorito.splice(index, 1);
+    
+  }
+
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favorito.length +1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favorito.push({...nuevoFavorito});
+    this.nuevoJuego = '';
   }
 
 }

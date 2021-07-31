e37da02d7971e2190df63b80fd29aee1b8dc053e
Jhonny Martínez Espinoza <jmartinezespza@gmail.com>
2021-07-31 12:26:12 -0500

    formulario templte: checkbox, radio, etc

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -8,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
+  persona = {
+    genero: 'F',
+    notificaciones: true
+  }
+
+  terminosYCondiciones: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {

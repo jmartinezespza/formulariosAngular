37f78e165ebdf4d7314e4a87f970451415722f07
Jhonny Martínez <jmartinezespza@gmail.com>
2021-07-31 19:14:33 -0500

    formularios reactivos inivial

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl('Laptop Thinkpad T480s'),
+  //   precio: new FormControl(1500),
+  //   existencias: new FormControl(5),
+  // })
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)] ],
+    precio: [, [ Validators.required, Validators.min(0) ]],
+    existencias: [, [ Validators.required, Validators.min(0) ] ],
+  })
+
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Laptop Thinkpad T480s',
+      precio: 1800
+    })
+  }
+
+  campoEsValido(nombre: string) {
+    return this.miFormulario.controls[nombre].errors && 
+            this.miFormulario.controls[nombre].touched
+  }
+
+  guardar() {
+
+    if ( this.miFormulario.invalid ) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
+
+    this.miFormulario.reset();
+    
   }
 
 }
